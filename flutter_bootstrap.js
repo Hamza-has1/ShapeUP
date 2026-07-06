72954a1a04207e1aeb72c1732f5aebe835fe93d2
Hamza <hamza@example.com>
2026-07-07 00:49:55 +0500

    Deploy ShapeUp update to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "953890635" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1444016886" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
